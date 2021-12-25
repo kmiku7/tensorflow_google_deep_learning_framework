365924e65258883b2246405e11472b9a9d903395
kmiku7 <kakoimiku@gmail.com>
2021-12-25 14:38:06 +0800

    Too lazy to write descriptive message

--- coordinator.py
+++ coordinator.py
@@ -1,1 +1,24 @@
 import tensorflow as tf
+import numpy as np
+import threading
+import time
+
+def my_loop(coord, worker_id):
+    while not coord.should_stop():
+        if np.random.rand() < 0.1:
+            print("Stopping from id: %d" % worker_id)
+            coord.request_stop()
+        else:
+            print("Working on id: %d" % worker_id)
+            time.sleep(1)
+
+coord = tf.train.Coordinator()
+threads = [
+    threading.Thread(target=my_loop, args=(coord, i,))
+    for i in range(5)
+]
+
+for t in threads:
+    t.start()
+
+coord.join(threads)
